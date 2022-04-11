947b9a0cdd2b174b1b73a62725ec8982a0e78960
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-11 16:52:43 +0200

    add app

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import store from "./app/store";
+import "antd/dist/antd.css";
+
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+      <App />{" "}
+      {/*That mean that every component inside app have access to variables inside store*/}
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
