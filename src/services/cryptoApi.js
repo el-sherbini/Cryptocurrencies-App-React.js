7a63b612a6d80027fb8c092122a349021564cbbc
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-05-20 17:58:52 +0200

    add non-private urls

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-  "x-rapidapi-host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
   "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
 };
 
@@ -9,7 +9,7 @@ const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://coinranking1.p.rapidapi.com" }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
